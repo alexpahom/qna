925b7ea94049670a1231ad1881c0995906a90e84
Alex Pahom <alex.pahomchik@gmail.com>
2023-10-12 18:49:48 +0200

    feature(uat): update models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   root to: 'questions#index'
 
   resources :questions do
-    resources :answers, shallow: true
+    resources :answers, shallow: true, except: %i[new index]
   end
 end
