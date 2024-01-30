d5795d40ae8f87f25f30dc7423dde0f57aea4fc6
Alex Pahom <alex.pahomchik@gmail.com>
2024-01-30 03:19:37 +0100

    feature(attach links): gist uploader

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
+  GIST_REGEXP = /^https:\/\/gist.github.com\/[a-zA-Z0-9]*\/[a-zA-Z0-9]*$/.freeze
+  GIST_ID_REGEXP = /^*.*\/([a-zA-Z0-9]*)$/.freeze
+
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
@@ -6,6 +9,14 @@ class Link < ApplicationRecord
 
   before_validation :append_scheme
 
+  def gist?
+    url.match? GIST_REGEXP
+  end
+
+  def gist_id
+    return unless gist?
+    url.match(GIST_ID_REGEXP)[-1]
+  end
   private
 
   def append_scheme
