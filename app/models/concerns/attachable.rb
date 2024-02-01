abfbaeafa716162b154a57f7191ce82770b6d7c8
Alex Pahom <alex.pahomchik@gmail.com>
2024-02-01 18:53:00 +0100

    feature(ajax): extract attaches to concern

---
+++ app/models/concerns/attachable.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Attachable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :links, dependent: :destroy, as: :linkable
+    has_many_attached :files
+
+    accepts_nested_attributes_for :links, reject_if: :all_blank
+  end
+end
