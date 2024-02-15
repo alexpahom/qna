b12352453f264df6d9e91778f0666956a4a3a7f2
Alex Pahom <alex.pahomchik@gmail.com>
2024-02-16 00:36:39 +0100

    feature(authorize): extend profiles api

--- spec/controllers/api/v1/profile_spec.rb
+++ spec/controllers/api/v1/profile_spec.rb
@@ -21,7 +21,7 @@ describe 'Profile API', type: :request do
 
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
         end
       end
 
@@ -33,5 +33,41 @@ describe 'Profile API', type: :request do
     end
   end
 
+  describe 'GET api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
 
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:users) { create_list(:user, 3) }
+      let(:me) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it_behaves_like 'Return successful'
+
+      it 'returns list of users without one' do
+        expect(json['users'].size).to eq(users.size - 1)
+      end
+
+      it 'does not return me' do
+        expect(json['users'].map(&:user['id'])).to_not include(me.id)
+      end
+
+      it_behaves_like 'Public fields' do
+        let(:attrs) { %w[id email admin created_at updated_at] }
+        let(:resource_response) { json['users'].last }
+        let(:resource) { users.last }
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
 end
--- spec/utils/shared/api_authorization.rb
+++ spec/utils/shared/api_authorization.rb
@@ -11,3 +11,17 @@ shared_examples_for 'API Authorizable' do
     end
   end
 end
+
+shared_examples_for 'Return successful' do
+  it 'return 200 status' do
+    expect(response).to be_successful
+  end
+end
+
+shared_examples 'Public fields' do
+  it 'return all public fields' do
+    attrs.each do |attr|
+      expect(resource_response[attr]).to eq resource.send(attr).as_json
+    end
+  end
+end
