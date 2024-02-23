e32b5877be42a2f070b14d47c19887127ea5062c
Alex Pahom <alex.pahomchik@gmail.com>
2024-02-23 12:42:41 +0100

    feature(search): minor fixes

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,32 +6,22 @@ RSpec.describe SearchController, type: :controller do
     let!(:questions) { create_list(:question, 3) }
     subject { Services::Search }
 
-    context 'valid attributes' do
-
-      Services::Search::SCOPES.each do |scope|
-        before do
-          allow(subject).to receive(:perform).and_return(questions)
-          get :index, params: { query: questions.shuffle.first.title, scope: scope }
-        end
-
-        it "#{scope} returns 200" do
-          expect(response).to be_successful
-        end
-
-        it "renders #{scope} index" do
-          expect(response).to render_template :index
-        end
-
-        it "#{scope} assign #perform to @results" do
-          expect(assigns(:results)).to eq questions
-        end
+    Services::Search::SCOPES.each do |scope|
+      before do
+        allow(subject).to receive(:perform).and_return(questions)
+        get :index, params: { query: questions.shuffle.first.title, scope: scope }
       end
-    end
 
-    context 'invalid attributes' do
-      it "renders index view" do
-        get :index, params: { query: questions.sample.title, scope: 'invalid' }
+      it "#{scope} returns 200" do
+        expect(response).to be_successful
+      end
+
+      it "renders #{scope} index" do
         expect(response).to render_template :index
+      end
+
+      it "#{scope} assign #perform to @results" do
+        expect(assigns(:results)).to eq questions
       end
     end
   end
