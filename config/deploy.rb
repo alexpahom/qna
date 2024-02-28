2dedc837716dbebb5cc86d45110622e33d6a0f1f
Alex Pahom <alex.pahomchik@gmail.com>
2024-02-28 01:48:47 +0100

    feature(cap): ruby v set

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :rvm_ruby, '3.2.1'
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
