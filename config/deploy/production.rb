14943b68e2b3d440b52bb7ef10db6f692e5c91c5
Alex Pahom <alex.pahomchik@gmail.com>
2024-02-29 06:51:14 +0100

    feature(cap): try fix css

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "64.225.97.221", user: "deployer", roles: %w{app db web}, primary: true
+server "46.101.114.184", user: "deployer", roles: %w{app db web}, primary: true
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
